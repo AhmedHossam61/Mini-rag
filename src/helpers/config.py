7282d4a1099c568efb0fc74e13b6aa8e85117fa7
AhmedHossam61 <ahmedhoss.2023@gmail.com>
2026-04-11 01:32:45 +0200

    feat: implement Pydantic-based configuration management with environment variable support

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,8 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings , SettingsConfigDict
 
 class Settings(BaseSettings):
-    app_name: str = "mini-rag"
-    app_version: str = "0.0.1"
+    app_name: str 
+    app_version: str 
     class Config:
         env_file = ".env"
-        
+
+def get_settings():
+    return Settings()
